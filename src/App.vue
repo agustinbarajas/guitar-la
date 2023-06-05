0c9229a6f13515f8d3c352121891e7ba8925c4a9
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-06-05 16:49:37 -0600

    feat: Add guitar list design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { Ref, onMounted, ref } from "vue";
+import GuitarCard from "./components/GuitarCard.vue";
+import { guitars as staticGuitars } from "./data";
+import { Guitar } from "./types";
 
+const guitars: Ref<Guitar[]> = ref([]);
+
+onMounted(() => {
+  guitars.value = staticGuitars;
+});
 </script>
 
 <template>
-  <h1>Guitar LA</h1>
-</template>
+  <main class="flex flex-col w-full">
+    <h2 class="text-center text-4xl font-bold">Guitar collection</h2>
 
-<style scoped></style>
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-5"
+    >
+      <GuitarCard v-for="guitar of guitars" :guitar="guitar"></GuitarCard>
+    </div>
+  </main>
+</template>
